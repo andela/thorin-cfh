b3587dc3a7c947929c0178fa0c91974c248bee9d
Chuzksy <chuzksy-codeactive>
2018-04-23 17:46:57 +0100

    chore(setup): setup test for frontend fix answers.js code to agree to ESLint rules [Fixes #156882152]

--- app/controllers/answers.js
+++ app/controllers/answers.js
@@ -1,32 +1,33 @@
+/* eslint-disable */
+
 /**
  * Module dependencies.
  */
-const mongoose = require('mongoose'),
-  Answer = mongoose.model('Answer'),
+const mongoose = require('mongoose');
 
+const Answer = mongoose.model('Answer');
 
-/**
- * Find answer by id
- */
- exports.answer = function (req, res, next, id) {
+// Find answer by id
+
+exports.answer = function (req, res, next, id) {
   Answer.load(id, (err, answer) => {
     if (err) return next(err);
-    if (!answer) return next(new Error('Failed to load answer ' + id));
+    if (!answer) return next(new Error(`Failed to load answer ${  id}`));
     req.answer = answer;
     next();
   });
 };
 
-/**
- * Show an answer
- */
+
+// Show an answer
+
 exports.show = function (req, res) {
   res.jsonp(req.answer);
 };
 
-/**
- * List of Answers
- */
+
+// List of Answers
+
 exports.all = function (req, res) {
   Answer.find({ official: true }).select('-_id').exec((err, answers) => {
     if (err) {
@@ -39,9 +40,9 @@ exports.all = function (req, res) {
   });
 };
 
-/**
- * List of Answers (for Game class)
- */
+
+// List of Answers (for Game class)
+
 exports.allAnswersForGame = function (cb) {
   Answer.find({ official: true }).select('-_id').exec((err, answers) => {
     if (err) {
