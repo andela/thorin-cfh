d89fe7b01d27391a9c16ef1dd9fe751cd3780c33
sunday taiwo <taiwo.sunday@outlook.comm>
2018-04-26 18:57:22 +0100

    feature(signpapi):Users should receive a JWT on successful signup
    - server returns jwt token in authentication response
    - moving from sessions to stateless authentication
    - user automatically get signin once they signup
    - check if email does not already exist in the dababase
    [Finishes #156882156]

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -7,6 +7,8 @@
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 
+require('dotenv').config({ path: '.env' });
+
 const User = mongoose.model('User');
 require('dotenv').config({ path: '.env' });
 
@@ -110,6 +112,39 @@ exports.create = function (req, res, next) {
   } else {
     return res.redirect('/#!/signup?error=incomplete');
   }
+};
+
+
+exports.createUser = function (req, res) {
+  const {
+    username, password, email, imageUrl, publicId
+  } = req.body;
+  const newUser = new User(req.body);
+  newUser.provider = 'local';
+  newUser.save(function (err) {
+    if (err) {
+      return res.status(409).json({
+        error: 'Error',
+        message: 'A user with that email already exist',
+        err
+      });
+    }
+    const user = {
+      _id: newUser._id, // eslint-disable-line no-underscore-dangle
+      imageUrl: newUser.imageUrl,
+      username: newUser.username
+    };
+    const token = jwt.sign({ user }, process.env.SECRET);
+    res.status(201).json({
+      message: 'Success',
+      user: {
+        _id: newUser._id, // eslint-disable-line no-underscore-dangle
+        username: newUser.username,
+        imageUrl: newUser.imageUrl
+      },
+      token
+    });
+  });
 };
 
 
--- config/env/all.js
+++ config/env/all.js
@@ -1,8 +1,6 @@
-const path = require('path');
+import path from 'path';
 
 const rootPath = path.normalize(`${__dirname}/../..`);
-
-// let keys = `${rootPath  }/keys.txt`;
 
 require('dotenv').config({ path: '.env' });
 
