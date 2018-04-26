ff8786aed74887b4303e83ef6b4ebb32841199de
Orlayhemmy <daminomics@gmail.com>
2018-04-26 18:03:45 +0100

    ft(signup)add image upload on signup or social media profile picture on authentication • Installed packages • add env file • store social media app secrets and Ids in .env • disable avatar selection on signup • refactor user controller on server-side to es6

--- test/user/model.js
+++ test/user/model.js
@@ -1,47 +1,49 @@
-// /**
-//  * Module dependencies.
-//  */
-// var should = require('should'),
-//     app = require('../../server'),
-//     mongoose = require('mongoose'),
-//     User = mongoose.model('User');
 
-// //Globals
-// var user;
+/* eslint-disable */
+/**
+ * Module dependencies.
+ */
+let should = require('should'),
+  app = require('../../server'),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
 
-// //The tests
-// describe('<Unit Test>', function() {
-//     describe('Model User:', function() {
-//         before(function(done) {
-//             user = new User({
-//                 name: 'Full name',
-//                 email: 'test@test.com',
-//                 username: 'user',
-//                 password: 'password'
-//             });
+// Globals
+let user;
 
-//             done();
-//         });
+// The tests
+describe('<Unit Test>', () => {
+  describe('Model User:', () => {
+    before((done) => {
+      user = new User({
+        name: 'Full name',
+        email: 'test@test.com',
+        username: 'user',
+        password: 'password'
+      });
 
-//         describe('Method Save', function() {
-//             it('should be able to save whithout problems', function(done) {
-//                 return user.save(function(err) {
-//                     should.not.exist(err);
-//                     done();
-//                 });
-//             });
+      done();
+    });
 
-//             it('should be able to show an error when try to save witout name', function(done) {
-//                 user.name = '';
-//                 return user.save(function(err) {
-//                     should.exist(err);
-//                     done();
-//                 });
-//             });
-//         });
+    describe('Method Save', () => {
+      it('should be able to save whithout problems', (done) => {
+        return user.save(function (err) {
+          should.not.exist(err);
+          done();
+        });
+      });
 
-//         after(function(done) {
-//             done();
-//         });
-//     });
-// });
+      it('should be able to show an error when try to save witout name', (done) => {
+        user.name = '';
+        return user.save(function (err) {
+          should.exist(err);
+          done();
+        });
+      });
+    });
+
+    after((done) => {
+      done();
+    });
+  });
+});
