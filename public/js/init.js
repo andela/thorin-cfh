b197948b05f8fc4b4ece2fe3f017584bb8aada44
Orlayhemmy <daminomics@gmail.com>
2018-04-27 19:53:24 +0100

    ft(signup)add image upload on signup or social media profile picture on user authentication
    installed packages
    •	add env file
    •	store social media app secrets and Ids in .env
    •	disable avatar selection on signup
    •	refactor user controller on server-side to es6

--- public/js/init.js
+++ public/js/init.js
@@ -16,7 +16,14 @@ const checkAuthenticationStatus = (token) => {
     return localStorage.setItem('card-game-token', '');
   }
   window.user = decoded.user;
+
+  if (window.location.pathname === "/auth/google/callback"
+    || window.location.pathname === "/auth/facebook/callback"
+    || window.location.pathname === "/auth/twitter/callback"
+    || window.location.pathname === "/auth/github/callback"){
+    window.location = '/';
+  }
+  
 };
 
 checkAuthenticationStatus(token);
-
