a2ba5d8b070526c807c16cf44ccc07fdea5d7cb6
Orlayhemmy <daminomics@gmail.com>
2018-05-04 08:44:03 +0100

    chore(github) - Remove github from codebase - Refactor passport.js - Rename env config file to js - Remove github api test

---
+++ config/env/test.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+module.exports = {
+  port: 3001,
+  app: {
+    name: 'Cards for Humanity - Test'
+  },
+  facebook: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  },
+  twitter: {
+    clientID: 'CONSUMER_KEY',
+    clientSecret: 'CONSUMER_SECRET',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+  },
+  google: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  }
+};
--- config/routes.js
+++ config/routes.js
@@ -50,15 +50,6 @@ module.exports = function (app, passport) {
     failureRedirect: '/signin'
   }), index.render);
 
-  // Setting the github oauth routes
-  app.get('/auth/github', passport.authenticate('github', {
-    failureRedirect: '/signin'
-  }), users.signin);
-
-  app.get('/auth/github/callback', passport.authenticate('github', {
-    failureRedirect: '/signin'
-  }), index.render);
-
   // Setting the twitter oauth routes
   app.get('/auth/twitter', passport.authenticate('twitter', {
     failureRedirect: '/signin'
