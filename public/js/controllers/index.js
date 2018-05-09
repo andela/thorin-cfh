2ae8b980dabefb6564ecae82a0659e61b590b66d
chika onuchukwu <chuzksy-codeactive>
2018-05-09 16:40:34 +0100

    “”

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,71 +1,79 @@
 /* eslint-disable */
-angular.module('mean.system')
-  .controller('IndexController', ['$scope', 'Global', '$http', '$q', '$location', 'socket', 'game', 
- ($scope, Global, $http, $q, $location, socket, game ) => {
+angular.module('mean.system').controller('IndexController', [
+  '$scope',
+  'Global',
+  '$http',
+  '$q',
+  '$location',
+  'socket',
+  'game',
+  ($scope, Global, $http, $q, $location, socket, game) => {
     $scope.global = Global;
+    $scope.username = null;
 
-    $scope.playAsGuest = function () {
+    $scope.playAsGuest = function() {
       game.joinGame();
       $location.path('/app');
     };
 
-    $scope.showError = function () {
+    $scope.showError = function() {
       if ($location.search().error) {
         return $location.search().error;
       }
       return false;
     };
 
-
     $scope.login = () => {
-      return $q.all([
-        $http.post('/api/auth/login', $scope.user)
-      ])
-        .then((successResponse) => {
+      return $q.all([$http.post('/api/auth/login', $scope.user)]).then(
+        successResponse => {
           $scope.global.authenticated = true;
           $scope.global.user = successResponse[0].data.data.user;
-          localStorage.setItem('card-game-token', successResponse[0].data.data.token);
+          localStorage.setItem(
+            'card-game-token',
+            successResponse[0].data.data.token
+          );
           window.user = successResponse[0].data.data.user;
           $location.path('/');
-        }, (errorResponse) => { 
+        },
+        errorResponse => {
           $scope.global.authenticated = false;
           $scope.global.user = null;
-          $scope.error = "Invaild Email or Password"
-        });
+          $scope.error = 'Invaild Email or Password';
+        }
+      );
     };
 
-  const progressBar = (data) => {
-      let elem = document.getElementById("move").style.width = data; 
-  }
-   
+    const progressBar = data => {
+      let elem = (document.getElementById('move').style.width = data);
+    };
 
     $scope.submit = () => {
       const file = document.getElementById('file').files[0];
       const fd = new FormData();
       fd.append('file', file);
       fd.append('upload_preset', 'cghgame');
-      $http.post('/api/auth/checkuser', $scope.user)
-      .then((res) => { 
-
-        if (res.status === 200) {
+      $http.post('/api/auth/checkuser', $scope.user).then(
+        res => {
+          if (res.status === 200) {
             axios
-            .post(
-              'https://api.cloudinary.com/v1_1/skybound/image/upload',
-              fd, {
-                onUploadProgress: (progressEvent) => {
-                  const level = `${Math.round(progressEvent.loaded /
-                   progressEvent.total * 100)}%`;
-                   $scope.progress = level;
-                   progressBar($scope.progress)
+              .post(
+                'https://api.cloudinary.com/v1_1/skybound/image/upload',
+                fd,
+                {
+                  onUploadProgress: progressEvent => {
+                    const level = `${Math.round(
+                      progressEvent.loaded / progressEvent.total * 100
+                    )}%`;
+                    $scope.progress = level;
+                    progressBar($scope.progress);
+                  }
                 }
-              }
-            )
-            .then((res) => {
-              $scope.user.imageUrl = res.data.secure_url;
-              $scope.user.publicId = res.data.public_id;
-              $http.post('/api/auth/signup', $scope.user)
-                .then(
-                  (res) => {
+              )
+              .then(res => {
+                $scope.user.imageUrl = res.data.secure_url;
+                $scope.user.publicId = res.data.public_id;
+                $http.post('/api/auth/signup', $scope.user).then(
+                  res => {
                     $scope.data = res.data;
                     $scope.global.authenticated = true;
                     $scope.global.user = $scope.data.user;
@@ -73,52 +81,51 @@ angular.module('mean.system')
                     window.user = $scope.data.user;
                     $location.path('/');
                   },
-                  (error) => {
+                  error => {
                     $scope.error = error.data;
                     $scope.global.authenticated = false;
                     $scope.global.user = null;
                     window.user = null;
-                    $scope.error = error.data.message
+                    $scope.error = error.data.message;
                   }
                 );
-            })
-            .catch((err) => {
-              $scope.error = 'Unable to upload. Check your internet'
-            });
+              })
+              .catch(err => {
+                $scope.error = 'Unable to upload. Check your internet';
+              });
           }
-      },(error) =>  { 
-        $scope.error = error.data.error.toString();
-      })
+        },
+        error => {
+          $scope.error = error.data.error.toString();
+        }
+      );
+    };
 
-    };    
-
-
-      $scope.imageUpload = (event) => {
-          let files = event.target.files;
-          for (let i = 0; i < files.length; i++) {
-              let file = files[i];
-              const reader = new FileReader();
-              reader.onload = $scope.imageIsLoaded;
-              reader.readAsDataURL(file);
-          }
+    $scope.imageUpload = event => {
+      let files = event.target.files;
+      for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+        const reader = new FileReader();
+        reader.onload = $scope.imageIsLoaded;
+        reader.readAsDataURL(file);
       }
+    };
 
-      $scope.imageIsLoaded = (e) => {
-          $scope.$apply(() => {
-              $scope.img = e.target.result;            
-          });
-      }
+    $scope.imageIsLoaded = e => {
+      $scope.$apply(() => {
+        $scope.img = e.target.result;
+      });
+    };
 
     $scope.disableSubmit = () => {
-        document.getElementById("submit").disabled = true;
-        function activateButton(element) {
-            if(element.checked) {
-              document.getElementById("submit").disabled = false;
-             }
-             else  {
-              document.getElementById("submit").disabled = true;
-            }
-      
+      document.getElementById('submit').disabled = true;
+      function activateButton(element) {
+        if (element.checked) {
+          document.getElementById('submit').disabled = false;
+        } else {
+          document.getElementById('submit').disabled = true;
         }
-    }
-  }]);
+      }
+    };
+  }
+]);
