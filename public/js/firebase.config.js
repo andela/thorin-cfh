233f2438de26baa740a59cd7149354d593d1af9c
Olusola Oseni <kenolusola@gmail.com>
2018-05-14 04:21:03 +0100

    feat(chat) implement chat features -ensure all users can chat in the game [Delivers #156882166]

---
+++ public/js/firebase.config.js
@@ -0,0 +1,11 @@
+// Firebase Initialization
+const config = {
+  apiKey: 'AIzaSyA_D02N4Ic5FfsGi0O2FiRM6o2SRWILu68',
+  authDomain: 'thorin-cfh-chat.firebaseapp.com',
+  databaseURL: 'https://thorin-cfh-chat.firebaseio.com',
+  projectId: 'thorin-cfh-chat',
+  storageBucket: 'thorin-cfh-chat.appspot.com',
+  messagingSenderId: '899785037614'
+};
+
+firebase.initializeApp(config); //eslint-disable-line
