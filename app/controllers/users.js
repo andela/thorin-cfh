a56d247012c415c1eda9ae5bb4948aaebefe480e
chika onuchukwu <chuzksy-codeactive>
2018-05-17 15:10:26 +0100

    “”

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -381,7 +381,7 @@ exports.game = function (req, res) {
 
     if (userGameDetails.length === 0) {
       return res.status(200).json({
-        message: 'You have not played a game',
+        message: 'You are yet to win a point',
         code: 200
       });
     }
